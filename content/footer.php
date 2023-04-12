2c85c60f6f9ae8df99fd16a8525dcd24d3af79ee
RubenPimz <126894612+RubenPimz@users.noreply.github.com>
2023-04-13 00:57:54 +0100

    add bootstrap / css / js /google fonts/ faviconlink

--- content/footer.php
+++ content/footer.php
@@ -1,4 +1,8 @@
 </main>
 <footer></footer>
+<!-- BOOTSRAP JS -->
+<script src="js/bootsrap.bundle.min.js"></script>
+<!-- MEU JS -->
+<script src="js/main.js"></script>
 </body>
 </html>
--- content/head.php
+++ content/head.php
@@ -4,6 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Index</title>
+    <link rel="shortcut icon" href="/img/favicon/favicon.png" type="image/png">
+    <!-- GOOGLE FONTS - ROBOT -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <!-- BOOTSRAP CSS -->
+    <link rel="stylesheet" href="css/boostrap.min.css">
+    <!-- BOOTSRAP ICONS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
+    <!-- MEU CSS -->
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
